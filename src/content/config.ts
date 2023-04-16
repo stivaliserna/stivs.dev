3ab2f334397cbe4366c9abff4dd55fb11edc44b5
Stivali <stivaliserna08@gmail.com>
2023-04-16 22:20:13 +0200

    Add open graph metadata to posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,7 @@ const postsCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string(),
+    image: z.string().optional(),
     pubDate: z.date().transform((date) => date.toLocaleDateString("en-US", {
       year: "numeric",
       month: "short",
